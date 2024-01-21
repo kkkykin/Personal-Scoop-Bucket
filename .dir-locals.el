8d49fa08227675deae11660b856c36ebfb8e6661
kkky@bow <kkkykin@foxmail.com>
2024-01-21 16:31:47 +0800

    add cloudflarest

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((json-ts-mode . ((json-ts-mode-indent-offset . 4))))
